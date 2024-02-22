3a82cd76ec00c1389da4536da7df6797a258b692
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-22 15:15:27 +0530

    Update notg_tb.v

--- verilog/Day1/Not_gate/notg_tb.v
+++ verilog/Day1/Not_gate/notg_tb.v
@@ -1,1 +1,23 @@
-
+module notgate_tb;//module name declaration
+   //input and output declaration
+  reg a;
+  wire b;
+  
+  not_gate tb(.a(a),.b(b));//instantiation(connects input and ouput of design and tb)
+  
+ //initial block for the code
+  initial begin
+    a=0;
+    #10 a=1;
+    #10 a=0;
+    #10
+    $finish;
+  end
+  
+   //second initial block for monitoring and dumping
+  initial begin
+    $monitor("%t | a= %d | b=%d",$time,a,b);
+    $dumpfile("not_gate.vcd");
+    $dumpvars(1,notgate_tb);
+  end
+endmodule//end of the module
