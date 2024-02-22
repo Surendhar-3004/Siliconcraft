4c493ba43cbe109df9796d4d0686f6952a240f0a
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-22 12:18:54 +0530

    Update andg_tb.v

--- verilog/Day1/And_gate/andg_tb.v
+++ verilog/Day1/And_gate/andg_tb.v
@@ -1,1 +1,24 @@
-
+module And_gate_tb;//module name declaration
+ //input and output declaration
+reg A, B;
+wire Y;
+  
+  AND_gate tb (.Y(Y), .A(A), .B(B));//instantiation(connects input and ouput ofdesign and tb)
+ 
+ //initial block for the code
+initial begin
+     A = 0; B = 0;
+ #10 A = 0; B = 1;
+ #10 A = 1; B = 0;
+ #10 A = 1; B = 1;
+ #10
+  $finish;
+end
+  
+ //second initial block for monitoring and dumping
+initial begin
+    $monitor ("%t | A = %d| B = %d| Y = %d", $time, A, B, Y);
+  $dumpfile("And_gate.vcd");
+  $dumpvars(0,And_gate_tb);
+end
+endmodule//end of the module
