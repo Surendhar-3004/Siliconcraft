82c4e09ce6ccf75a4e49a780a3427f7cf03ea6f7
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-22 17:12:20 +0530

    Update full_adder_tb.v

--- verilog/Day1/Full_adder/full_adder_tb.v
+++ verilog/Day1/Full_adder/full_adder_tb.v
@@ -1,1 +1,26 @@
-
+module full_adder_tb;
+  reg a,b,cin;
+  wire sum,carry;
+  
+  full_adder full_adder2(
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .carry(carry));
+  
+   initial begin
+     a=0;b=0;cin=0;
+     #10 a=0;b=0;cin=1;
+     #10 a=0;b=1;cin=0;
+     #10 a=0;b=1;cin=1;
+     #10
+     $finish;
+   end
+    
+    initial begin
+      $monitor("%t | a=%d | b=%d | cin=%d | sum=%d | carry=%d",$time,a,b,cin,sum,carry);
+      $dumpfile("full_adder.vcd");
+      $dumpvars(1,full_adder_tb);
+    end
+    endmodule
