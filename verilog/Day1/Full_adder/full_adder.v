a2ae8859ff057d9811509341245846d5dffde609
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-22 17:10:03 +0530

    Update full_adder.v

--- verilog/Day1/Full_adder/full_adder.v
+++ verilog/Day1/Full_adder/full_adder.v
@@ -1,1 +1,23 @@
-
+module full_adder
+  (input a,
+   input b,
+   input cin,
+   output sum,
+   output carry,
+   wire x,y,z);
+  
+  
+  halfadder h1(.a(a),.b(b),.s(x),.c(y));
+  halfadder h2(.a(x),.b(cin),.s(sum),.c(z));
+  or(carry,y,z);
+  
+endmodule:full_adder
+  
+  module halfadder
+    (input a,
+     input b,
+     output s,
+     output c);
+    xor(s,a,b);
+    and(c,a,b);
+  endmodule:halfadder
