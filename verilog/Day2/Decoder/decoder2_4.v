fa57477b54128c81e964fe2ad7bef992b87fbaa7
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-23 22:18:59 +0530

    Update decoder2_4.v

--- verilog/Day2/Decoder/decoder2_4.v
+++ verilog/Day2/Decoder/decoder2_4.v
@@ -1,1 +1,11 @@
-
+module decoder2_4
+  (input a,b,
+   output y0,y1,y2,y3);
+  
+  not nt1(n0,a);
+  not nt2(n1,b);
+  and a1(y0,n0,n1);
+  and a2(y1,n0,b);
+  and a3(y2,a,n1);
+  and a4(y3,a,b);
+endmodule
