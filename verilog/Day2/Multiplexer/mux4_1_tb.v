c436d2f31521c5123a275ea70f69efa3b9cc716f
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-23 17:15:39 +0530

    Update mux4_1_tb.v

--- verilog/Day2/Multiplexer/mux4_1_tb.v
+++ verilog/Day2/Multiplexer/mux4_1_tb.v
@@ -1,1 +1,21 @@
-
+module mux4_1_tb;
+  reg a,b,c,d,s0,s1;
+  wire y;
+  
+  mux4_1 mux1(.a(a),.b(b),.c(c),.d(d),.s0(s0),.s1(s1),.y(y));
+  
+  initial begin
+    a=0;b=1;c=0;d=1;s0=0;s1=0;
+    #20 s0=0;s1=1;
+    #20 s0=1;s1=0;
+    #20 s0=1;s1=1;
+    #20
+    $finish;
+  end
+  
+  initial begin
+    $monitor("time=%0t | a=%0b | b=%b | c=%b | d=%b |  s0=%b | s1=%b | y=%b",$time,a,b,c,d,s0,s1,y);
+    $dumpfile("mux4_1.vcd");
+    $dumpvars(1,mux4_1_tb);
+  end 
+endmodule
