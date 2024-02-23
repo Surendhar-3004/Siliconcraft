00650063cbf9f42fbd9dcda1d0f559beadbbcfb4
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-23 17:19:10 +0530

    Update demux1_4_tb.v

--- verilog/Day2/Demultiplexer/demux1_4_tb.v
+++ verilog/Day2/Demultiplexer/demux1_4_tb.v
@@ -1,1 +1,24 @@
-
+module demux1_4_tb;
+  reg d,s0,s1;
+  wire y0,y1,y2,y3;
+  
+  demux1_4 demux(.d(d),.s0(s0),.s1(s1),.y0(y0),.y1(y1),.y2(y2),.y3(y3));
+  
+  initial begin
+    d=1;s0=0;s1=0;
+    #10 s0=0;s1=1;
+   
+    #10 s0=1;s1=0;
+   
+    #10 s0=1;s1=1;
+    
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | d=%b | s0=%d | s1=%d | y0=%d | y1=%d | y2=%d | y3=%d",$time,d,s0,s1,y0,y1,y2,y3); 
+    $dumpfile("demux1_4.vcd");
+    $dumpvars(1,demux1_4_tb);
+  end 
+endmodule
