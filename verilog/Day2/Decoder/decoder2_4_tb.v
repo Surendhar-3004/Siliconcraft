a2296bfd2e83dffed9568306f3f8adaa460748eb
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-23 22:20:03 +0530

    Update decoder2_4_tb.v

--- verilog/Day2/Decoder/decoder2_4_tb.v
+++ verilog/Day2/Decoder/decoder2_4_tb.v
@@ -1,1 +1,21 @@
-
+module decoder2_4_tb;
+  reg a,b;
+  wire y0,y1,y2,y3;
+  
+  decoder2_4 decoder(.a(a),.b(b),.y0(y0),.y1(y1),.y2(y2),.y3(y3));
+  
+  initial begin
+    a=0;b=0;
+    #10 a=0;b=1;
+    #10 a=1;b=0;
+    #10 a=1;b=1;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b | b=%b | y0=%b | y1=%b | y2=%b | y3=%b",$time,a,b,y0,y1,y2,y3);
+    $dumpfile("decoder2_4.vcd");
+    $dumpvars(1,decoder2_4_tb);
+  end
+endmodule
