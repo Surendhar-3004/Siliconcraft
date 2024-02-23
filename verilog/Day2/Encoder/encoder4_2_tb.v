ebac8eee65b5b5e9e4da21a8c4611500602e45ec
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-23 22:22:20 +0530

    Update encoder4_2_tb.v

--- verilog/Day2/Encoder/encoder4_2_tb.v
+++ verilog/Day2/Encoder/encoder4_2_tb.v
@@ -1,1 +1,21 @@
-
+module encoder4_2_tb;
+  reg a,b,c,d;
+  wire y0,y1;
+  
+  encoder4_2 encoder(.a(a),.b(b),.c(c),.d(d),.y0(y0),.y1(y1));
+  
+  initial begin
+    a=0;b=0;c=0;d=1;
+    #10 a=0;b=0;c=1;d=0;
+    #10 a=0;b=1;c=0;d=0;
+    #10 a=1;b=0;c=0;d=0;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b |  b=%b |  c=%b |  d=%b |  y0=%b |  y1=%b",$time,a,b,c,d,y0,y1);
+    $dumpfile("encoder4_2.vcd");
+    $dumpvars(1,encoder4_2_tb);
+  end
+endmodule
