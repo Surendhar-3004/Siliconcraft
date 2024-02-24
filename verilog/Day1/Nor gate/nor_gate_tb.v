92d477f84eb1063829bd08097ca48bdb35ed3c26
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-24 19:57:14 +0530

    Update nor_gate_tb.v

--- verilog/Day1/Nor gate/nor_gate_tb.v
+++ verilog/Day1/Nor gate/nor_gate_tb.v
@@ -1,1 +1,24 @@
-
+module nor_gate_tb;//module name declaration
+ //input and output declaration
+reg A, B;
+wire Y;
+  
+  nor_gate nor_gate2 (.Y(Y), .A(A), .B(B));//instantiation(connects input and ouput ofdesign and tb)
+ 
+ //initial block for the code
+initial begin
+     A = 0; B = 0;
+ #10 A = 0; B = 1;
+ #10 A = 1; B = 0;
+ #10 A = 1; B = 1;
+ #10
+  $finish;
+end
+  
+ //second initial block for monitoring and dumping
+initial begin
+    $monitor ("%t | A = %d| B = %d| Y = %d", $time, A, B, Y);
+  $dumpfile("nor_gate.vcd");
+  $dumpvars(0,nor_gate_tb);
+end
+endmodule//end of the module
