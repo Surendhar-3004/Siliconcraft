6cac8e1af40b6daa06e8d85ff183062e47d40cd1
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-24 20:05:49 +0530

    Update xnor_gate_tb.v

--- verilog/Day1/Xnor_gate/xnor_gate_tb.v
+++ verilog/Day1/Xnor_gate/xnor_gate_tb.v
@@ -1,1 +1,28 @@
+module xnorg_tb;//module name declaration
+   //input and output declaration
+  reg A, B;
+  wire Y;
+  xnor_gate xnorgate(.A(A), .B(B), .Y(Y));//instantiation(connects input and ouput ofdesign and tb)
+  
+ //initial block for the code
+  initial begin
+    $dumpfile("xnor_gate.vcd");
+    $dumpvars(1, xnorg_tb);  
+  end
 
+   //second initial block for monitoring and dumping
+  initial begin
+    A = 1'b0; B = 1'b0;
+    #10 A = 1'b0; B = 1'b1;
+    #10 A = 1'b1; B = 1'b0;
+    #10 A = 1'b1; B = 1'b1;
+    #10
+    $finish;
+  end
+
+  initial begin
+  $monitor("time=%0t | A=%b | B=%b | Y = %b", $time, A, B, Y);
+  $dumpfile("xnor_gate.vcd");
+  $dumpvars(1,xnorg_tb);
+  end
+endmodule//end of the module
