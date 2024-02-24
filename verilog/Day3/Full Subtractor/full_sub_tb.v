3cd9d7c8ff57814b8f7e6d0073492ad86968487c
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-24 19:47:53 +0530

    Update full_sub_tb.v

--- verilog/Day3/Full Subtractor/full_sub_tb.v
+++ verilog/Day3/Full Subtractor/full_sub_tb.v
@@ -1,1 +1,26 @@
-
+module full_sub_tb;
+  reg a,b,cin;
+  wire diff,borrow;
+  
+  full_sub fs(.a(a),.b(b),.cin(cin),.diff(diff),.borrow(borrow));
+  
+  initial begin
+    a=0;b=0;cin=0;
+    #10 a=0;b=0;cin=1;
+    #10 a=0;b=1;cin=0;
+    #10 a=0;b=1;cin=1;
+    #10 a=1;b=0;cin=0;
+    #10 a=1;b=0;cin=1;
+    #10 a=1;b=1;cin=0;
+    #10 a=1;b=1;cin=1;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b | b=%b | cin=%d | diff=%b | borrow=%b",$time,a,b,cin,diff,borrow);
+    $dumpfile("full_sub.vcd");
+    $dumpvars(1,full_sub_tb);
+    
+  end
+endmodule
