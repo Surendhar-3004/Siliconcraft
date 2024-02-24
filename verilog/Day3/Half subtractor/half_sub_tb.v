de35f0ffb2c4b12256931b7765fd35595ae720cd
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-24 19:30:31 +0530

    Update half_sub_tb.v

--- verilog/Day3/Half subtractor/half_sub_tb.v
+++ verilog/Day3/Half subtractor/half_sub_tb.v
@@ -1,1 +1,22 @@
-
+module half_sub_tb;
+  reg a,b;
+  wire diff,borrow;
+  
+  half_sub hs(.a(a),.b(b),.diff(diff),.borrow(borrow));
+  
+  initial begin
+    a=0;b=0;
+    #10 a=0;b=1;
+    #10 a=1;b=0;
+    #10 a=1;b=1;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b | b=%b | diff=%b | borrow=%b",$time,a,b,diff,borrow);
+    $dumpfile("half_sub.vcd");
+    $dumpvars(1,half_sub_tb);
+    
+  end
+endmodule
