3c57237db36ef8f929ee955a538234ec77534324
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-24 19:47:30 +0530

    Update full_sub.v

--- verilog/Day3/Full Subtractor/full_sub.v
+++ verilog/Day3/Full Subtractor/full_sub.v
@@ -1,1 +1,15 @@
-
+module full_sub
+  (input a,b,cin,
+   output diff,borrow);
+  
+  wire p,q,r,s,t;
+  
+  xor x1(p,a,b);
+  xor x2(diff,p,cin);
+  not n1(q,a);
+  and a1(t,q,b);
+  not n2(r,p);
+  and a2(s,r,cin);
+  or (borrow,s,t);
+  
+endmodule
