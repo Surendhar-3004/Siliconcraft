60281db7f88aafad9995e87e9c6fc6f3c537bc1d
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-25 23:00:08 +0530

    Update comparator.v

--- verilog/Day4/Comparator/comparator.v
+++ verilog/Day4/Comparator/comparator.v
@@ -1,1 +1,19 @@
-
+module comparator
+  (input a,b,
+   output reg equal,lesser,greater);
+  
+always@* begin
+  equal=0;
+  lesser=0;
+  greater=0;
+  
+  if(a>b)
+    greater=1;
+  
+    else if(a<b) 
+      lesser=1;
+    
+    else
+      equal=1;
+  end
+endmodule
