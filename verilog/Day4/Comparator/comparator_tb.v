add4fcbc2b28af3af5a80a102a58a3fa2b8da36a
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-25 23:00:24 +0530

    Update comparator_tb.v

--- verilog/Day4/Comparator/comparator_tb.v
+++ verilog/Day4/Comparator/comparator_tb.v
@@ -1,1 +1,21 @@
-
+module comparator_tb;
+  reg a,b;
+  wire equal,lesser,greater;
+  
+  comparator cmp(.a(a),.b(b),.equal(equal),.lesser(lesser),.greater(greater));
+  
+  initial begin
+    a=0;b=0;
+    #10 a=0;b=1;
+    #10 a=1;b=0;
+    #10 a=1;b=1;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b | b=%b | equal=%b | lesser=%b | greater=%b",$time,a,b,equal,lesser,greater);
+    $dumpfile("comparator.vcd");
+    $dumpvars(1,comparator_tb);
+  end
+endmodule
