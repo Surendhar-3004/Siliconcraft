1adaa2676d35380c2bfc37661d0f568c2f84537e
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-26 16:32:53 +0530

    Update d flipflop_tb.v

--- verilog/Day5/D flipflop/d flipflop_tb.v
+++ verilog/Day5/D flipflop/d flipflop_tb.v
@@ -1,1 +1,22 @@
+module d_flipflop_tb;
+  reg d,clk,reset;
+  wire q;
+  
+  d_flipflop df(.d(d),.clk(clk),.reset(reset),.q(q));
+  
+  initial begin
+    clk=0;reset=1;d=1;
 
+    #20 $finish;
+  end
+  
+      always #4 clk = ~clk;
+  	  always #8 d = ~d;
+      always #4 reset = ~reset;
+  
+  initial begin
+    $monitor("%t | d=%b | clk=%b | reset=%b | q=%b",$time,d,clk,reset,q);
+    $dumpfile("d_flipflop.vcd");
+    $dumpvars(1,d_flipflop_tb);
+  end
+endmodule
