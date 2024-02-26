378b31d2ea7cb06d4531840feba6a1973c5879e6
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-26 10:15:30 +0530

    Update excess3_bcd_tb.v

--- verilog/Day5/Excess3_Bcd/excess3_bcd_tb.v
+++ verilog/Day5/Excess3_Bcd/excess3_bcd_tb.v
@@ -1,1 +1,62 @@
-
+module excess3_bcd_tb;
+  reg e3,e2,e1,e0;
+  wire b3,b2,b1,b0;
+  
+  excess3_bcd eb(.e3(e3),.e2(e2),.e1(e1),.e0(e0),.b3(b3),.b2(b2),.b1(b1),.b0(b0));
+  
+  initial begin
+    e3=0;
+    e2=0;
+    e1=1;
+    e0=1;
+    #10
+    e3=1;
+    e2=0;
+    e1=1;
+    e0=1;
+	#10 
+    e3=0;
+    e2=1;
+    e1=0;
+    e0=0;
+    #10 
+    e3=1;
+    e2=0;
+    e1=1;
+    e0=0;
+    #10 
+    e3=0;
+    e2=1;
+    e1=0;
+    e0=0;
+    #10
+    e3=1;
+    e2=0;
+    e1=1;
+    e0=1;
+    #10
+    e3=0;
+    e2=1;
+    e1=0;
+    e0=0;
+    #10
+    e3=1;
+    e2=0;
+    e1=1;
+    e0=0;
+    #10
+    e3=0;
+    e2=1;
+    e1=1;
+    e0=1;
+    #10
+    
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | e3=%b | e2=%b | e1=%b | e0=%b | b3=%b | b2=%b | b1=%b | b0=%b |",$time,e3,e2,e1,e0,b3,b2,b1,b0);
+    $dumpfile("excess3_bcd.vcd");
+    $dumpvars(1,excess3_bcd_tb);
+  end
+endmodule
