83c55f73b412e2b2e37217f243de0a457735c44c
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-27 23:05:57 +0530

    Update cmos_inverter_tb.v

--- verilog/Day6/cmos_inverter/cmos_inverter_tb.v
+++ verilog/Day6/cmos_inverter/cmos_inverter_tb.v
@@ -1,1 +1,21 @@
-
+module inverter_tb;
+  reg in;
+  wire out;
+  
+  inverter inv(.in(in),.out(out));
+  
+  initial begin
+    in=0;
+    #10 in=1;
+    #10 in=0;
+    #10 in=1;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | in=%b | out=%b",$time,in,out);
+    $dumpfile("inverter.vcd");
+    $dumpvars(1,inverter_tb);
+  end
+endmodule
