d8f5717232ff376976b4b79afd2c1f68e660c11a
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-27 11:17:13 +0530

    Update d_latch_tb.v

--- verilog/Day6/d_latch/d_latch_tb.v
+++ verilog/Day6/d_latch/d_latch_tb.v
@@ -1,1 +1,23 @@
-
+module d_latch_tb;
+  reg d,en,reset;
+  wire q;
+  
+  d_latch dl(.d(d),.en(en),.reset(reset),.q(q));
+  
+  initial begin
+    en=0;reset=1;d=0;
+     #2 reset=0;
+     #20 
+    $finish;
+  end
+  
+      always #5 d = ~d;
+  	  always #2 en = ~en;
+     
+  
+  initial begin
+    $monitor("%t | d=%b | en=%b | reset=%b | q=%b",$time,d,en,reset,q);
+    $dumpfile("d_latch.vcd");
+    $dumpvars(1,d_latch_tb);
+  end
+endmodule
