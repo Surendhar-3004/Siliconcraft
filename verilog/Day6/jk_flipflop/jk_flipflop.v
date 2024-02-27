af26bc9392f79121f19816e5a30eaa0c0fcc96e9
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-27 16:54:07 +0530

    Update jk_flipflop.v

--- verilog/Day6/jk_flipflop/jk_flipflop.v
+++ verilog/Day6/jk_flipflop/jk_flipflop.v
@@ -1,1 +1,19 @@
-
+module jk_flipflop
+  (input j,k,clk,
+   output reg q);
+  
+  initial
+    q=0;
+  always @(posedge clk)
+        begin 
+      if(clk==0)
+        q=q;
+          else if( j==0 && k==0)
+        q=q;
+          else if( j==1 && k==1)
+        q=~q;
+      
+      else 
+        q=j;
+    end
+endmodule
