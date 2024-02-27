c57d1546649d56f19dd9b2e87d1f570210129916
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-27 16:20:44 +0530

    Update sr_flipflop.v

--- verilog/Day6/sr_flipflop/sr_flipflop.v
+++ verilog/Day6/sr_flipflop/sr_flipflop.v
@@ -1,1 +1,19 @@
-
+module sr_flipflop
+  (input s,r,clk,
+   output reg q);
+  
+  initial 
+    q=0;
+  always @(posedge clk)
+    begin 
+      if(clk==0)
+        q=q;
+      else if( s==0 && r==0)
+        q=q;
+      else if( s==1 && r==1)
+        q=1'bx;
+      
+      else 
+        q=s;
+    end
+endmodule
