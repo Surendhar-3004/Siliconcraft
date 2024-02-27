6c70e60a08089280016c04f3a623fb054ac3b556
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-27 16:21:04 +0530

    Update sr_flipflop_tb.v

--- verilog/Day6/sr_flipflop/sr_flipflop_tb.v
+++ verilog/Day6/sr_flipflop/sr_flipflop_tb.v
@@ -1,1 +1,23 @@
-
+module sr_flipflop_tb;
+  reg s,r,clk;
+  wire q;
+  
+  sr_flipflop srf(.s(s),.r(r),.clk(clk),.q(q));
+  
+  initial begin
+    clk=0;s=0;r=1;
+     #20 
+    $finish;
+  end
+  
+      always #4 s = ~s;
+  	  always #6 r= ~r;
+  	  always #2 clk = ~clk;
+     
+  
+  initial begin
+    $monitor("%t | s=%b | r=%b | clk=%b | q=%b",$time,s,r,clk,q);
+    $dumpfile("sr_flipflop.vcd");
+    $dumpvars(1,sr_flipflop_tb);
+  end
+endmodule
