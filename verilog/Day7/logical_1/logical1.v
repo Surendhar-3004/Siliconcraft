76306cc581fbc37abb7df75a61147cac0b90841b
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 22:42:28 +0530

    Update logical1.v

--- verilog/Day7/logical_1/logical1.v
+++ verilog/Day7/logical_1/logical1.v
@@ -1,1 +1,16 @@
+module logical_operators(
+  input [3:0]a,b,c,d,
+  output reg x,y);
 
+  always @(*) begin
+	if ((a==b)&&(c==b)) 
+	x=1;   
+	else
+	x=!a;
+
+	if((a!=d)||(b==c))
+	y=1;                
+	else
+	y=0;                
+  end
+endmodule
