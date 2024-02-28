21e88045e6fa3c14fc737922f828d8d13e9f419c
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 15:05:26 +0530

    Update arithmetic_tb.v

--- verilog/Day7/Arithmetic/arithmetic_tb.v
+++ verilog/Day7/Arithmetic/arithmetic_tb.v
@@ -1,1 +1,18 @@
-
+module arithmetic_tb;
+  reg [5:0]a,b;
+  wire [7:0] y1,y2,y3,y4,y5;
+  
+  arithmetic arith(.a(a),.b(b),.y1(y1),.y2(y2),.y3(y3),.y4(y4),.y5(y5));
+  
+  initial begin
+    a=9;b=3;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%d | b=%d | y1=%d | y2=%d | y3=%d | y4=%d | y5=%d |",$time,a,b,y1,y2,y3,y4,y5);
+    $dumpfile("arithmetic.vcd");
+    $dumpvars(1,arithmetic_tb);
+  end
+endmodule
