0f173219ffb3996176bc2c44001085513da148af
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 23:06:46 +0530

    Update con_rep_tb.v

--- verilog/Day7/concatenation_replication/con_rep_tb.v
+++ verilog/Day7/concatenation_replication/con_rep_tb.v
@@ -1,1 +1,19 @@
-
+module con_rep_tb;
+  reg [3:0]a,b;
+  wire [7:0] y,z;
+  
+  con_rep cr(.a(a),.b(b),.y(y),.z(z));
+  
+  initial begin
+    a=4'b0101;
+    b=4'b00x1;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b | b=%b | y=%b | z=%b",$time,a,b,y,z);
+    $dumpfile("con_rep.vcd");
+    $dumpvars(1,con_rep_tb);
+  end
+endmodule
