95f0fa1bd052f537b9c826e9bc8adb04ab4ec126
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 23:20:53 +0530

    Update logical2_tb.v

--- verilog/Day7/logical_2/logical2_tb.v
+++ verilog/Day7/logical_2/logical2_tb.v
@@ -1,1 +1,11 @@
+module logical_operator(
+  input a,b,c,d,
+  output reg x1,x2,x3);
 
+  always @(*) begin
+    assign x1=a && b;
+    assign x2=c || d;
+    assign x3 = (!a);
+  end
+endmodule
+    
