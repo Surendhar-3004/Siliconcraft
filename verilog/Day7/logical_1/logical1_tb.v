57c147529e6ce6681aa5e694aa235548cc624e6f
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 22:42:49 +0530

    Update logical1_tb.v

--- verilog/Day7/logical_1/logical1_tb.v
+++ verilog/Day7/logical_1/logical1_tb.v
@@ -1,1 +1,21 @@
-
+module logical_tb;
+  reg [3:0] a,b,c,d;
+  wire x,y;
+  
+  logical_operators lo(.a(a),.b(b),.c(c),.d(d),.x(x),.y(y));
+  
+  initial begin
+    a=4'b1111;         
+	b=4'b1010;          
+	c=4'b1001;         
+	d=4'b1101;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b | b=%b | c=%b | d=%b | x=%b | y=%b",$time,a,b,c,d,x,y);
+    $dumpfile("logical_operators.vcd");
+    $dumpvars(1,logical_tb);
+  end
+endmodule
