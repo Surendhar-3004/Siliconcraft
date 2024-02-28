da2cbe81ef1115a892095f65faa7bc0d64cb7ef3
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 22:23:44 +0530

    Update bitwise.v

--- verilog/Day7/Bitwise/bitwise.v
+++ verilog/Day7/Bitwise/bitwise.v
@@ -1,1 +1,12 @@
-
+module bitwise
+  (input [7:0] q,b,
+   output reg[7:0] x1,x2,x3,x4,x5);
+  
+  always @(*) begin
+    assign x1= q & b;
+    assign x2= q | b;
+    assign x3= ~q;
+    assign x4= q ^ b;
+    assign x5= q ~^ b;
+  end
+endmodule
