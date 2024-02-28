5c3950e1e18b9f120007e41f564de1f063f9e3dd
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 23:41:00 +0530

    Update pargen_even_tb.v

--- verilog/Day7/parity_generator_even/pargen_even_tb.v
+++ verilog/Day7/parity_generator_even/pargen_even_tb.v
@@ -1,1 +1,21 @@
-
+module parity_gen_tb;
+  reg a,b,c;
+  wire p;
+  
+  parity_gen_even pe(.a(a),.b(b),.c(c),.p(p));
+  
+  initial begin
+    a=0;b=1;c=1;
+    #10 a=0;b=1;c=1;
+    #10 a=1;b=0;b=0;
+    #10 a=0;b=0;c=1;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b | b=%b | c=%b | p=%b",$time,a,b,c,p);
+    $dumpfile("parity_gen_even");
+    $dumpvars(1,parity_gen_tb);
+  end
+endmodule
