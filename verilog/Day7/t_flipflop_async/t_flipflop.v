a82ba082aeb6d5e8297be67cca5d51306b6cca04
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 07:11:08 +0530

    Update t_flipflop.v

--- verilog/Day7/t_flipflop_async/t_flipflop.v
+++ verilog/Day7/t_flipflop_async/t_flipflop.v
@@ -1,1 +1,14 @@
-
+ module t_flipflop (
+  t,clk,reset ,q
+  );
+  input t, clk, reset ; 
+  output q;
+  reg q;
+  always @ ( posedge clk or negedge reset)
+  if (~reset) begin
+    q <= 1'b0;
+  end else if (t) begin
+    q <=  ! q;
+  end
+  
+  endmodule
