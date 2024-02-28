1e613a5d47b78ae1f5438e381813611aee8cb6ff
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 19:41:40 +0530

    Update d_flipflop_async_tb.v

--- verilog/Day7/d_flipflop_async/d_flipflop_async_tb.v
+++ verilog/Day7/d_flipflop_async/d_flipflop_async_tb.v
@@ -1,1 +1,22 @@
-
+module dff_async_tb;
+  reg d,clk,reset;
+  wire  q;
+  
+  dff_async dff(.d(d),.clk(clk),.reset(reset),.q(q));
+  
+  initial begin
+    d=0;clk=0;reset=1;
+    #20
+    $finish;
+  end
+  
+  always #4 d=~d;
+  always #4 clk=~clk;
+  always #2 reset=~reset;
+  
+  initial begin
+    $monitor("%t | d=%b | clk=%b | reset=%b | q=%b",$time,d,clk,reset,q);
+    $dumpfile("dff_async.vcd");
+    $dumpvars(1,dff_async_tb);
+  end
+endmodule
