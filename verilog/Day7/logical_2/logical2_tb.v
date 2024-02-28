5bd2afbc63842307f251fe79d09d9fd0a9abdfef
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-28 23:21:48 +0530

    Update logical2_tb.v

--- verilog/Day7/logical_2/logical2_tb.v
+++ verilog/Day7/logical_2/logical2_tb.v
@@ -1,11 +1,21 @@
-module logical_operator(
-  input a,b,c,d,
-  output reg x1,x2,x3);
-
-  always @(*) begin
-    assign x1=a && b;
-    assign x2=c || d;
-    assign x3 = (!a);
+module logical_operator_tb;
+  reg a,b,c,d;
+  wire x1,x2,x3;
+  
+  logical_operator lo(.a(a),.b(b),.c(c),.d(d),.x1(x1),.x2(x2),.x3(x3));
+  
+  initial begin
+    a=1;         
+	b=0;          
+	c=1;         
+	d=1;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%b | b=%b | c=%b | d=%b | x1=%b | x2=%b | x3",$time,a,b,c,d,x1,x2,x3);
+    $dumpfile("logical_operator.vcd");
+    $dumpvars(1,logical_operator_tb);
   end
 endmodule
-    
