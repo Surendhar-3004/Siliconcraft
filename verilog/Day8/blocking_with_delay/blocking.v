8722976c4663e5ec5e5e53d1d27121c493e38448
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-29 22:27:30 +0530

    Update blocking.v

--- verilog/Day8/blocking_with_delay/blocking.v
+++ verilog/Day8/blocking_with_delay/blocking.v
@@ -1,1 +1,19 @@
-
+module tb;
+  reg[7:0]a,b,c,d,e;
+  initial begin
+    a=8'hDA;
+    $display("[%0t] a=0x%0h b=0x%0h c=0x%0h",$time,a,b,c);
+    #15
+    b=8'hf1;
+    $display("[%0t] a=0x%0h b=0x%0h c=0x%0h",$time,a,b,c);
+    c=8'h30;
+    $display("[%0t] a=0x%0h b=0x%0h c=0x%0h",$time,a,b,c);
+  end
+  initial begin
+    d=8'hAA;
+    $display("[%0t] d=0x%0h e=0x%0h ",$time,d,e);
+    #10
+    e=8'h55;
+    $display("[%0t] d=0x%0h e=0x%0h ",$time,d,e);
+  end
+endmodule
