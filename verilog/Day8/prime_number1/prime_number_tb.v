4d6d8cf63fc1a835fd375c11905ffc7ce02b3cc6
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-29 22:09:00 +0530

    Update prime_number_tb.v

--- verilog/Day8/prime_number1/prime_number_tb.v
+++ verilog/Day8/prime_number1/prime_number_tb.v
@@ -1,1 +1,19 @@
-
+module prime_number_tb;
+  reg a;
+  reg count;
+  wire y;
+  
+  prime_number pn(.a(a),.count(count),.y(y));
+  
+  initial begin
+    repeat(4)
+    a=5;count=0;
+     #3  a=8;count=0;
+        #3 a=12;count=0;
+    
+    $monitor("a=%0d",a);
+   
+    #100
+    $finish;
+  end 
+endmodule
