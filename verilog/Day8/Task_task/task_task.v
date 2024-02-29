e217cd4c941006fde1dcdc1503f54af3e8ef8229
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-29 14:46:47 +0530

    Update task_task.v

--- verilog/Day8/Task_task/task_task.v
+++ verilog/Day8/Task_task/task_task.v
@@ -1,1 +1,21 @@
-
+module task1;
+  task add;
+    input integer a,b;
+    output integer s;
+    begin
+      s=a+b;
+      $display(s);
+    end
+  endtask
+  
+  task values;
+    begin
+    add(3,4);
+    add (4,5);
+    end
+  endtask
+  
+  initial begin
+    values;
+  end
+endmodule
