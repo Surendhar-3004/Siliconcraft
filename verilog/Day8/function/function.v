1d460693ee12bbd13cefaf3bb4818ab60487cb72
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-29 11:22:03 +0530

    Update function.v

--- verilog/Day8/function/function.v
+++ verilog/Day8/function/function.v
@@ -1,1 +1,11 @@
-
+module function_tb;
+  
+  function integer sum(integer a,b);
+    begin
+      sum=a+b;
+    end
+    endfunction
+      initial
+        $display("sum" ,sum(2,3));
+endmodule
+    
