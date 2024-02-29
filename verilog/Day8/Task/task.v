ec39a0f3f311b2fa653ff9544f0312d2e9c8f897
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-02-29 13:05:49 +0530

    Update task.v

--- verilog/Day8/Task/task.v
+++ verilog/Day8/Task/task.v
@@ -1,1 +1,12 @@
-
+module tasks;
+  integer ans;
+  task sum(integer a,b,output int c);
+    begin
+      c=a+b;
+    end
+  endtask
+      initial begin
+        sum(2,5,ans);
+        $display("ans=%0d",ans);
+  end
+endmodule
