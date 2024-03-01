5daf0e40e003b1321d1df71ccfbb8948a942531b
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-01 15:00:08 +0530

    Update prime_number.v

--- verilog/Day8/prime_number2/prime_number.v
+++ verilog/Day8/prime_number2/prime_number.v
@@ -1,1 +1,9 @@
-
+module prime_check(
+  input reg [7:0]a);
+  always @(*) begin
+    if((a==2)||(a%2==1)&&(a%3==1)||(a%3==2))
+      $display("It is a prime number");
+      else 
+        $display("It is not a prime number");
+    end
+endmodule
