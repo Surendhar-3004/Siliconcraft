d38d18f63bad204899b41088f44bb3003c8450fd
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-01 21:48:27 +0530

    Update even_number.v

--- verilog/Day9/Even_number/even_number.v
+++ verilog/Day9/Even_number/even_number.v
@@ -1,1 +1,10 @@
-
+module even_number(
+  input int a);
+  always @(*) begin
+    if(a%2==0)begin
+      $display("it is a even number");
+    end
+    else
+      $display("it is not a even number");
+  end
+endmodule
