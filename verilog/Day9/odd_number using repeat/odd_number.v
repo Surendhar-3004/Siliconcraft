5e514b6d6adf446f309b2049eed5b5fefb8a0fb4
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-01 22:49:27 +0530

    Update odd_number.v

--- verilog/Day9/odd_number using repeat/odd_number.v
+++ verilog/Day9/odd_number using repeat/odd_number.v
@@ -1,1 +1,10 @@
-
+module odd_number(
+  input int a);
+  
+  always @(*) begin 
+    if(a%2!=0)
+      $display(a," is a odd number");
+    else
+      $display(a," is not a odd number");
+  end
+endmodule
