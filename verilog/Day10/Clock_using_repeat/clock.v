aad099cd0ba19d457503b889348af9779c5f7c31
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-03 23:40:19 +0530

    Update clock.v

--- verilog/Day10/Clock_using_repeat/clock.v
+++ verilog/Day10/Clock_using_repeat/clock.v
@@ -1,1 +1,20 @@
-
+module clk_generation(
+ output reg clk);
+  
+initial begin
+  
+    clk=0;
+    #20
+    $finish;
+  end
+  
+  initial begin
+    repeat(5) begin
+      #2 clk=~clk;
+  end
+  end
+  
+  initial begin
+    $monitor("%t | clk=%b |",$time,clk);
+  end
+endmodule
