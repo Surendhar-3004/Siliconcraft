dec2342ab3541bb2c200480e867ebf9a259b7c8f
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-03 20:53:17 +0530

    Update swapping.v

--- verilog/Day10/Swapping/swapping.v
+++ verilog/Day10/Swapping/swapping.v
@@ -1,1 +1,11 @@
-
+module swapping(
+  input [10:0] a ,b,
+  output reg[10:0] swapped_a,swapped_b);
+  reg [10:0]temp;
+  
+  always @(*) begin
+    temp=a;
+    swapped_a = b;
+    swapped_b = temp;
+  end
+endmodule
