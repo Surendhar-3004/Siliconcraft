e1625de021f8186ef3588b16ac6ba5e7c9ae797d
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-03 23:35:37 +0530

    Update shift_register.v

--- verilog/Day10/shift_register using case statement/shift_register.v
+++ verilog/Day10/shift_register using case statement/shift_register.v
@@ -1,1 +1,14 @@
-
+module shift_register(
+  input [4:0] a,
+  input reg [1:0]s,
+  output reg[4:0] q);
+  
+  always @(*) begin
+    case(s)
+      2'b00: q=a;
+      2'b01: q=a<<1;
+      2'b10: q=a>>1;
+      default: q = 5'b00000; 
+      endcase
+  end
+endmodule
