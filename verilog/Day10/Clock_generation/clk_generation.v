6c8396fb6b4db782614b0fee245ea533954fda74
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-03 14:18:57 +0530

    Update clk_generation.v

--- verilog/Day10/Clock_generation/clk_generation.v
+++ verilog/Day10/Clock_generation/clk_generation.v
@@ -1,1 +1,17 @@
-
+module clk_generation(
+ output reg clk);
+  
+initial begin
+    clk=0;
+    #2
+    clk=1;
+    #3
+    clk=0;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | clk=%b |",$time,clk);
+  end
+endmodule
