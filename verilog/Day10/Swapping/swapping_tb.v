a2705f5c399fb30c9d7923942566ba2df51516db
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-03 20:54:11 +0530

    Update swapping_tb.v

--- verilog/Day10/Swapping/swapping_tb.v
+++ verilog/Day10/Swapping/swapping_tb.v
@@ -1,1 +1,19 @@
-
+module swapping_tb;
+  reg [10:0] a,b;
+  wire [10:0] swapped_a,swapped_b;
+  
+  swapping swap(.a(a),.b(b),.swapped_a(swapped_a),.swapped_b(swapped_b));
+  
+  initial begin
+    a=10;
+    b=5;
+    #10
+    $finish;
+  end
+  
+  initial begin
+    $monitor("%t | a=%0d | b=%0d | swapped_a=%0d | swapped_b=%0d",$time,a,b,swapped_a,swapped_b);
+    $dumpfile("swapping.vcd");
+    $dumpvars(1,swapping_tb);
+  end
+endmodule
