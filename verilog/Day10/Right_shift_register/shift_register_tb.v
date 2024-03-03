c66d250158dd5c5f8c7bb39b4fddce3717d3a933
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-03 21:46:03 +0530

    Update shift_register_tb.v

--- verilog/Day10/Right_shift_register/shift_register_tb.v
+++ verilog/Day10/Right_shift_register/shift_register_tb.v
@@ -1,1 +1,17 @@
-
+module shift_register_tb;
+  reg [4:0] a;
+  wire [4:0] q;
+  
+  shift_register sr(.a(a),.q(q));
+  
+  initial begin
+    repeat(2) 
+    a=10101;
+    #10
+    $finish;
+  end
+  
+  always @* begin
+    $display("a=%b,q=%b",a,q);
+  end
+endmodule
