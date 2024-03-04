b1d4d1a902b3abaec0a293ce1234a3077ae026b5
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-04 14:56:38 +0530

    Update swapping2.v

--- verilog/Day10/Swapping/swapping2.v
+++ verilog/Day10/Swapping/swapping2.v
@@ -1,1 +1,15 @@
-
+module swapping;
+  int a=8,b=5;
+  
+  reg [4:0]temp;
+  
+  initial begin
+    temp=a;
+    a=b;
+    b=temp;
+  end
+  initial begin
+    $display("a=%0d,b=%0d",a,b);
+  end
+endmodule
+    
