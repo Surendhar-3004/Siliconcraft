46823aba125d6c7e642eab77641dcf3ad4862bd0
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-04 16:21:10 +0530

    Update reduction.v

--- verilog/Day11/Reduction/reduction.v
+++ verilog/Day11/Reduction/reduction.v
@@ -1,1 +1,13 @@
-
+module reduction(
+  input reg [4:0] a,
+  output reg y1,y2,y3,y4,y5,y6);
+  
+ always @(*) begin
+      y1=&a;
+      y2=|a;
+      y3=~&a;
+      y4=~|a;
+      y5=^a;
+      y6=^~a;
+    end
+endmodule
