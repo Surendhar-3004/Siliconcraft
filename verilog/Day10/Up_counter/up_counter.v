171a0400a0b6923e2a8734b925d51e94df5eef46
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-05 21:56:17 +0530

    Update up_counter.v

--- verilog/Day10/Up_counter/up_counter.v
+++ verilog/Day10/Up_counter/up_counter.v
@@ -1,1 +1,12 @@
-
+module up_counter(
+  input reg reset,clk,
+  output reg[3:0]y);
+  
+  always @(posedge clk)begin
+    if(reset)
+     y<=4'b0000;
+    else
+      y<=y+1;
+    
+  end
+endmodule
