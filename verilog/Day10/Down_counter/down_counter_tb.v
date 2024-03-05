9e90017e09255ec9ff12fea0c5cc378028dd4ed4
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-05 21:59:18 +0530

    Update down_counter_tb.v

--- verilog/Day10/Down_counter/down_counter_tb.v
+++ verilog/Day10/Down_counter/down_counter_tb.v
@@ -1,1 +1,24 @@
+module down_counter_tb;
+  reg reset,clk;
+  wire [3:0]y;
+  
+  down_counter down(.reset(reset),.clk(clk),.y(y));
 
+ 
+  initial begin
+    clk=1;
+     reset=1;
+   #20
+    reset=0;
+    #300
+   
+     $finish;
+  end
+ 
+     always #10 clk=~clk;
+
+  initial begin
+    $monitor("reset=%0b clk=%0b y=%0d",reset,clk,y);
+  end
+endmodule
+  
