df91493ac27682bff3c9b17ebb1d368a5f7dfa18
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-05 21:57:03 +0530

    Update up_counter_tb.v

--- verilog/Day10/Up_counter/up_counter_tb.v
+++ verilog/Day10/Up_counter/up_counter_tb.v
@@ -1,1 +1,23 @@
+module up_counter_tb;
+  reg reset,clk;
+  wire [3:0]y;
+  
+  up_counter up(.reset(reset),.clk(clk),.y(y));
 
+  always #10 clk=~clk;
+  initial begin
+    clk=1;
+     reset=1;
+   #20
+    reset=0;
+    #300
+   
+     $finish;
+  end
+ 
+    
+
+  initial begin
+    $monitor("reset=%0b clk=%0b y=%0d",reset,clk,y);
+  end
+endmodule
