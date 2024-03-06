b88e6658a6d2cf35349cec4f3a15d3ff148c9b0b
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-06 07:31:46 +0530

    Update ring_counter.v

--- verilog/Day_12/Ring_counter/ring_counter.v
+++ verilog/Day_12/Ring_counter/ring_counter.v
@@ -1,1 +1,11 @@
-
+module ring_counter(
+  input clk,reset,
+  output reg[1:0] y);
+  
+  always @(posedge clk) begin
+    if(reset || (y==2'b11))
+      y=2'b00;
+    else
+      y=y+1;
+  end
+endmodule
