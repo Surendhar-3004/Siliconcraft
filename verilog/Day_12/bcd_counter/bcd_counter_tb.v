f56d6e04f6578fa4d73d1684980f47b4720f9fbd
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-06 07:07:02 +0530

    Update bcd_counter_tb.v

--- verilog/Day_12/bcd_counter/bcd_counter_tb.v
+++ verilog/Day_12/bcd_counter/bcd_counter_tb.v
@@ -1,1 +1,20 @@
-
+module bcd_counter_tb;
+  reg clk,reset;
+  wire [3:0]y;
+  
+  bcd_counter bcd(.clk(clk),.reset(reset),.y(y));
+  
+  initial begin
+    reset=1;clk=0;
+    #10
+    reset=0;
+    #100
+    $finish;
+  end
+  
+  always #2clk=~clk;
+  
+  initial begin
+    $monitor("%t | clk=%0b | reset=%0b | y=%0d",$time,clk,reset,y);
+  end
+endmodule
