ff8ad9ca9ed754cc82e81caab75c057ff6ac5fde
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-06 06:29:13 +0530

    Update up_down.v

--- verilog/Day_12/Up_down_counter/up_down.v
+++ verilog/Day_12/Up_down_counter/up_down.v
@@ -1,1 +1,15 @@
-
+module up_down_counter(
+  input reset,clock,
+  input s,
+  output reg[3:0]y);
+  
+  always @(posedge clock) begin
+    if(reset)
+      y=4'b0000;
+    else
+    case(s)
+      0: y=y+1;
+      1: y=y-1;
+    endcase
+  end
+endmodule
