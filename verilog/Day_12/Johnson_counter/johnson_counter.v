861bbafc2553cba02ba78410dc820b470ce3cc61
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-06 13:45:03 +0530

    Update johnson_counter.v

--- verilog/Day_12/Johnson_counter/johnson_counter.v
+++ verilog/Day_12/Johnson_counter/johnson_counter.v
@@ -1,1 +1,19 @@
+module johnson_counter #(parameter width=4) (
+  input reg clk,
+  input reset,
+  output reg[width-1:0]cout);
+
+ integer i;
+  always @(posedge clk) begin
+    if (reset)
+      cout <= 4'b1100;
+    else 
+      cout[width-1]<=~cout[0];
+      for( i=0;i<width-1;i=i+1)
+      cout[i]=cout[i+1];
+  end    
+  initial begin
+    $monitor("clk=%d reset=%d out=%b",clk,reset, cout);
+  end
+endmodule
 
