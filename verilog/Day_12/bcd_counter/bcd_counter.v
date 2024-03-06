e267d1d0c0ff3ad34181976b855fa9cac2673a0d
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-06 07:06:32 +0530

    Update bcd_counter.v

--- verilog/Day_12/bcd_counter/bcd_counter.v
+++ verilog/Day_12/bcd_counter/bcd_counter.v
@@ -1,1 +1,11 @@
-
+module bcd_counter(
+  input clk,reset,
+  output reg[3:0]y);
+  
+  always @(posedge clk) begin
+    if(reset || (y>=9))
+      y<=4'b0000;
+  else
+    y<=y+1;
+  end
+endmodule
