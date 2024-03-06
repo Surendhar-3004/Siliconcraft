6aa3e10001da04d873aa859fbc23975df61b37af
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-06 07:32:10 +0530

    Update ring_counter_tb.v

--- verilog/Day_12/Ring_counter/ring_counter_tb.v
+++ verilog/Day_12/Ring_counter/ring_counter_tb.v
@@ -1,1 +1,21 @@
-
+module ring_counter_tb;
+  reg clk,reset;
+  wire [1:0] y;
+  
+  ring_counter ring(.clk(clk),.reset(reset),.y(y));
+  
+  initial begin
+    reset=1;clk=0;
+    #10
+    reset=0;
+    #300
+    $finish;
+    
+  end
+  
+  always #5 clk=~clk;
+  
+  initial begin
+    $monitor("%t | reset=%0b | clk=%0b | y=%b",$time,reset,clk,y);
+  end
+endmodule
