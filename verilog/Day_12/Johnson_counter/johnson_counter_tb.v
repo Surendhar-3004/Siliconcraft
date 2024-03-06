c2fc0c4d669a4bdd2289f83a2ad9db5b1a6f4106
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-06 13:45:23 +0530

    Update johnson_counter_tb.v

--- verilog/Day_12/Johnson_counter/johnson_counter_tb.v
+++ verilog/Day_12/Johnson_counter/johnson_counter_tb.v
@@ -1,1 +1,22 @@
+module johnson_counter_tb;
+parameter width=4;
+  reg clk,reset;
+  wire [3:0] cout;
+
+  johnson_counter aa (
+    .clk(clk),
+    .reset(reset),
+    .cout(cout)
+  );
+
+  always #5 clk = ~clk;
+
+  initial begin
+    clk = 0;
+    reset = 1;
+    #10 reset = 0;
+    #100 $finish;
+  end
+
+endmodule
 
