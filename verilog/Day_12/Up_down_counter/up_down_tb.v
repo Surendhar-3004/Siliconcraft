92afd82647682c7b598d320d6e53bfbada90d34b
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-06 06:29:37 +0530

    Update up_down_tb.v

--- verilog/Day_12/Up_down_counter/up_down_tb.v
+++ verilog/Day_12/Up_down_counter/up_down_tb.v
@@ -1,1 +1,25 @@
-
+module up_down_counter_tb;
+  reg reset,clock;
+  reg s;
+  wire [3:0]y;
+  
+  up_down_counter uut(.s(s),.reset(reset),.clock(clock),.y(y));
+  
+  initial begin
+    s=0;reset=1;clock=1;
+    #2 reset=0;
+    #300
+    s=1;reset=1;clock=1;
+    #2 reset=0;
+    #300
+    $finish;
+  end
+  
+  always #10clock=~clock;
+  always #10 s=~s;
+  
+  
+  always @(*) begin
+    $monitor("%t | s=%0b | reset=%0b | clock=%0b |y=%0d",$time,s,reset,clock,y);
+  end
+endmodule
