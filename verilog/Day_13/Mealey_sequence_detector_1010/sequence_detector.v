da3f192531784e94f0f41125c469b953e94669ee
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-11 17:13:34 +0530

    Update sequence_detector.v

--- verilog/Day_13/Mealey_sequence_detector_1010/sequence_detector.v
+++ verilog/Day_13/Mealey_sequence_detector_1010/sequence_detector.v
@@ -1,1 +1,55 @@
-
+module mealey_1010(
+  input in,clk,reset,
+  output reg [1:0] present,next,
+  output reg out);
+  
+  parameter [1:0] s0=2'b00,
+                  s1=2'b01,
+                  s2=2'b10,
+       			  s3=2'b11;
+  
+  always @(posedge clk)begin
+    if(reset)
+      present<=2'b00;
+  else
+    present<=next;
+  end
+  
+  always @(in)
+    begin
+      case(present)
+        s0:
+          begin
+          out=0;
+            if(in==1)
+            next<=s1;
+        else
+          next<=s0;
+          end
+        s1:
+          begin
+          out=0;
+            if(in==0)
+              next<=s2;
+            else
+              next<=s1;
+          end
+        s2:
+          begin
+            out=0;
+            if(in==1)
+              next<=s3;
+            else
+              next<=s0;
+          end
+        s3:
+          begin
+            out=1;
+            if(in==0)
+              next<=s0;
+            else
+              next<=s0;
+          end
+      endcase
+    end
+  endmodule
