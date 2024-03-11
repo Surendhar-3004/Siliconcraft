dbe4529f01ebfae72be660e2f1d9c493332c1359
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-11 15:49:56 +0530

    Update sequence_detector_tb.v

--- verilog/Day_13/Moore_sequence_detector_1001/sequence_detector_tb.v
+++ verilog/Day_13/Moore_sequence_detector_1001/sequence_detector_tb.v
@@ -1,1 +1,32 @@
-
+module sequence_detector_tb;
+  reg clk,reset,in;
+  wire [2:0]present,next;
+  wire out;
+  
+  moore_sequence_detector_1001 moore(.clk(clk),.reset(reset),.in(in),.present(present),.next(next),.out(out));
+  
+  initial begin
+    clk=1;reset=1;
+    #5
+    reset=0;
+    end
+  initial begin
+    #10
+    in=1'b1;
+    #10
+    in=1'b0;
+    #10
+    in=1'b0;
+    #10
+    in=1'b1;
+    #100
+    $finish;
+  end
+  
+  always #5 clk=~clk;
+ 
+  
+  initial begin
+    $monitor("%t | reset=%b | clk=%b | in=%b | present=%b | next=%b | out=%b",$time,reset,clk,in,present,next,out);
+  end
+endmodule
