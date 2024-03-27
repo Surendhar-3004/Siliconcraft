e954393aecc7d684319c58d192fb9266c19d449a
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-03-27 16:15:09 +0530

    Update sram.v

--- verilog/Day_15/SRAM/sram.v
+++ verilog/Day_15/SRAM/sram.v
@@ -1,1 +1,16 @@
-
+module memory(
+  input clk,
+  input reset,
+  input write,
+  input [2:0]data_in,
+  input [4:0]addr,
+  output reg [2:0]data_out);
+  
+  reg [2:0]ram[15:0];
+  always @(posedge clk) begin
+     if(write)
+      ram[addr]<=data_in;
+    else
+      data_out<=ram[addr];
+  end
+endmodule
