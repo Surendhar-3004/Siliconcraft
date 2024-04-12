5c8a574c6bb0400d621a2b492be82b10287a8535
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-04-12 22:36:59 +0530

    Update 3D_packedarray.v

--- System_verilog/Day1/3D_packedarray.v
+++ System_verilog/Day1/3D_packedarray.v
@@ -1,1 +1,18 @@
-
+module three_D_packedarray;
+  
+  bit[2:0][3:0][7:0] data;
+  
+  initial begin
+    data[0]=32'hcafe_face;
+    data[1]=32'hc0de_fade;
+    data[2]=32'h2345_6789;
+    
+    $display("The data's are %0h",data);
+    $display("");
+    
+    foreach(data[i])begin
+      $display("The data at [%0d] is [%0h]",i,data[i]);
+    end
+    $display("");
+  end
+endmodule
