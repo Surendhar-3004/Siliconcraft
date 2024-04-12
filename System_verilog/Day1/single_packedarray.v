5c6677f56916576e61e9f2d0f10ad9d3dde6d750
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-04-12 22:19:07 +0530

    Update single_packedarray.v

--- System_verilog/Day1/single_packedarray.v
+++ System_verilog/Day1/single_packedarray.v
@@ -1,1 +1,13 @@
-
+module single_packed_array_tb;
+  
+  bit [7:0] array;
+  
+  initial begin
+    array=8'hb5;
+  end
+  
+  initial begin
+    for(int i=0; i<$size(array): i=i+1)
+      $display("array[%0d]=%0b,i,array[i]);
+               end
+               endmodule
