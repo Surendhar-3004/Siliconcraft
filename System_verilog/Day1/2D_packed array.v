4fff0e131844ba207783a0c27f33afe0316dbfb3
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-04-12 22:00:22 +0530

    Update 2D_packed array.v

--- System_verilog/Day1/2D_packed array.v
+++ System_verilog/Day1/2D_packed array.v
@@ -1,1 +1,15 @@
-
+module Two_D_packedarray;
+  
+  bit[3:0] [7:0] array_value;
+  
+  initial begin
+    array_value=32'h0102_0304;
+  
+    $display ("array_value=0x%h",array_value);
+  end
+  
+  initial begin
+    for(int i=0;i<=3; i++)
+      $display("The value at [	%0d]=%h",i,array_value[i]);
+  end
+endmodule
