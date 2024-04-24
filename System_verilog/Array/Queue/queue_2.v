32be06b8058154a7a29cdc26d31b955e04df4fa4
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-04-24 21:51:32 +0530

    Update queue_2.v

--- System_verilog/Array/Queue/queue_2.v
+++ System_verilog/Array/Queue/queue_2.v
@@ -1,1 +1,28 @@
-
+module queue;
+  int j=1;
+  int q[$]={0,2,5};
+ 
+  initial begin
+	q.insert(1,j);
+    $display("%p",q);
+    q.delete(1);
+    $display(q);
+    q.push_front(7);
+    $display(q);
+    q.push_back(9);
+    q.pop_back();
+     $display(q);
+    q.pop_front();
+     $display(q);
+    q.reverse();
+     $display(q);
+    q.sort();
+     $display(q);
+     $display(q);
+    q.rsort();
+     $display(q);
+    q.shuffle();
+     $display(q);
+    
+  end
+endmodule
