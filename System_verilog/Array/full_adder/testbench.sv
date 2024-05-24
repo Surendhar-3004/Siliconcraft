f23239522f0ff6ed8ec1eac1453ca511183c205f
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-05-24 22:22:27 +0530

    Update testbench.sv

--- System_verilog/Array/full_adder/testbench.sv
+++ System_verilog/Array/full_adder/testbench.sv
@@ -1,1 +1,27 @@
+`include "Test.sv"
+`include "Interface.sv"
 
+module testbench;
+  inter in();
+  test ts;
+  mailbox m;
+  full_adder fa(.a(in.a),
+                .b(in.b),
+                .cin(in.cin),
+                .sum(in.sum),
+                .cout(in.cout));
+  
+  initial begin
+    m=new();
+    ts=new(in,m);
+    fork
+      ts.memory;
+      ts.run;
+    join
+  end
+ initial begin
+   #5;
+   $display("a=%d b=%d cin=%d sum=%d cout=%d",in.a,in.b,in.cin,in.sum,in.cout);
+ end
+  
+endmodule
