3c1164e0acfe18fea66664006f524452cfcb0bda
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-05-24 22:21:34 +0530

    Update test.sv

--- System_verilog/Array/full_adder/test.sv
+++ System_verilog/Array/full_adder/test.sv
@@ -1,1 +1,27 @@
+`include "Environment.sv"
 
+class test;
+  transaction t;
+  environment env;
+  mailbox m;
+  virtual inter vi;
+  
+  function new(virtual inter vi,mailbox m);
+    this.vi=vi;
+    this.m=m;
+  endfunction
+  
+  task memory;
+    t=new();
+    env=new(vi,m);
+  endtask
+  
+  task run;
+    
+      fork
+    env.memory;
+    env.run;
+      join
+    
+  endtask
+endclass
