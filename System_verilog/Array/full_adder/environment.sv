7a9cb0c19c933967ad4db8435054256e6e5dda6b
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-05-24 22:20:56 +0530

    Update environment.sv

--- System_verilog/Array/full_adder/environment.sv
+++ System_verilog/Array/full_adder/environment.sv
@@ -1,1 +1,30 @@
+`include "Transaction.sv"
+`include "Driver.sv"
+`include "Generator.sv"
 
+class environment;
+  driver d;
+  generator g;
+  virtual inter vi;
+  mailbox m;
+  transaction t;
+  
+  function new(virtual inter vi,mailbox m);
+    this.vi=vi;
+    this.m=m;
+  endfunction
+  
+  task memory;
+    t=new();
+    g=new(m);
+    d=new(vi,m);
+  endtask
+  
+  task run;
+    fork
+    g.gene;
+    d.dri;
+    join
+  endtask
+  
+endclass
