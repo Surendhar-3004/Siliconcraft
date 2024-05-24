e455de441ba91de3e16a110a5d684ac7eeb4870e
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-05-24 22:19:25 +0530

    Update generator.sv

--- System_verilog/Array/full_adder/generator.sv
+++ System_verilog/Array/full_adder/generator.sv
@@ -1,1 +1,28 @@
 
+class generator;
+  transaction t;
+  mailbox m;
+  
+  function new(mailbox m);
+    this.m=m;
+  endfunction
+  
+  task gene;
+    
+    repeat (5) begin
+  
+    t=new();
+    t.randomize();
+//     t.a=1'b1;
+//     t.b=1'b1;
+//     t.cin=1'b1;
+    m.put(t);
+    $display("a=%d b=%d cin=%d ",t.a,t.b,t.cin);
+    $display("packe is sent to driver");
+    end
+    
+  endtask
+    
+endclass
+
+
