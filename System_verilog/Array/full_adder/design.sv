c87f270c2f4f2a67cf21760cd1be1c9240ad8cec
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-05-24 22:16:54 +0530

    Update design.sv

--- System_verilog/Array/full_adder/design.sv
+++ System_verilog/Array/full_adder/design.sv
@@ -1,1 +1,10 @@
-
+module full_adder(input a,
+                 input b,
+                 input cin,
+                 output wire sum,
+                  output wire cout);
+  assign sum=a^b^cin;
+  assign cout=(a&b) | (b&cin) | (a&cin);
+  
+endmodule
+               
