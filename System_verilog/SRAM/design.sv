f5ede597d6bfaf2b6b9b75865971641e3af51e2d
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 15:24:36 +0530

    Update design.sv

--- System_verilog/SRAM/design.sv
+++ System_verilog/SRAM/design.sv
@@ -1,1 +1,19 @@
+module sram(input clk,
+          //  input reset,
+            input [3:0]wdata,
+            input [3:0]addr,
+           input w_en,
+            output reg [3:0]rdata);
+  reg [3:0]mem[7:0];
+  
+  always @(posedge clk)begin
+//     if(reset==1)
+//       rdata<=0;
+     if(w_en==1)
+      mem[addr]<=wdata;
+    else if(w_en==0)
+      rdata<=mem[addr];
+  end
+  
+endmodule
 
