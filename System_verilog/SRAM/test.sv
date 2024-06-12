8b46f35c333e4c2b0b063ee8d99a2f3e6f7c807e
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 15:37:46 +0530

    Update test.sv

--- System_verilog/SRAM/test.sv
+++ System_verilog/SRAM/test.sv
@@ -1,1 +1,25 @@
-
+`include "Environment.sv"
+class test;
+  environment env;
+  mailbox mail;
+  transaction t;
+  virtual inter vi;
+    
+    function new(virtual inter vi,mailbox mail);
+      this.vi=vi;
+      this.mail=mail;
+    endfunction
+      
+      task memory;
+        t=new();
+        env=new(vi,mail);
+      endtask
+      
+      task run;
+        fork
+        env.memory;
+        env.run;
+        join
+      endtask
+    
+    endclass
