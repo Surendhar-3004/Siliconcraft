5715414d7fda4516825ddd257ffe6732aa38610c
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 15:37:27 +0530

    Update environment.sv

--- System_verilog/SRAM/environment.sv
+++ System_verilog/SRAM/environment.sv
@@ -1,1 +1,31 @@
+`include "Transaction.sv"
+`include "Generator.sv"
+`include "Driver.sv"
 
+class environment;
+  mailbox mail;
+  virtual inter vi;
+    transaction t;
+    generator g;
+    driver d;
+    
+    function new(virtual inter vi,mailbox mail);
+      this.vi=vi;
+      this.mail=mail;
+    endfunction
+      
+      task memory;
+        t=new();
+        g=new(mail);
+        d=new(vi,mail);
+      endtask
+      
+      task run;
+        fork
+        g.gen;
+        d.drive;
+        join
+      endtask
+      
+      
+endclass
