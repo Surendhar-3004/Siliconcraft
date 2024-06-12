5e07422de2615ee54469910f55eb912aee6e418b
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 17:05:56 +0530

    Update fork_join_none_using_fork_join_any.sv

--- System_verilog/SV codes/fork_join_none_using_fork_join_any/fork_join_none_using_fork_join_any.sv
+++ System_verilog/SV codes/fork_join_none_using_fork_join_any/fork_join_none_using_fork_join_any.sv
@@ -1,1 +1,29 @@
-
+// Code your testbench here
+// or browse Examples
+module testmodule;
+  
+  initial begin
+    
+    int completed_blocks=0;
+    
+    fork
+      begin
+        $display("start of first parallel block");
+        #10;
+        $display("end of first parallel block");
+        completed_blocks++;
+      end
+      
+      begin
+        $display("start of second parallel block");
+        #20;
+        $display("end of second parallel block");
+        completed_blocks++;
+      end
+    
+    join_none
+    repeat (2) @(posedge $time);
+    
+    $display("code after the fork_join construct");
+  end
+endmodule
