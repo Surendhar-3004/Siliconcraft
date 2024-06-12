fe2aebc431123585c937b8316d5225a660238440
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 15:51:18 +0530

    Update event_fork_join.sv

--- System_verilog/SV codes/event_fork_join/event_fork_join.sv
+++ System_verilog/SV codes/event_fork_join/event_fork_join.sv
@@ -1,1 +1,29 @@
-
+module event_module;
+  event e1;
+  
+  task process_A();
+    $display("@%0t: 1",$time);
+    ->e1;
+    $display("@%0t: 2",$time);
+  endtask
+  
+  task process_B();
+    $display("@%0t: 3",$time);
+    @e1;
+    $display("@%0t: 4",$time);
+  endtask
+  
+  task process_c();
+    $display("@%0t: 5",$time);
+    wait(e1.triggered);
+    $display("@%0t: 6",$time);
+  endtask
+  
+  initial begin
+    fork
+      process_A();
+      process_B();
+      process_c();
+    join
+  end
+endmodule
