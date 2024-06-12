1843fa978e27d2a92eef0debd802ff67157953b9
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 15:36:00 +0530

    Update generator.sv

--- System_verilog/SRAM/generator.sv
+++ System_verilog/SRAM/generator.sv
@@ -1,1 +1,20 @@
-
+class generator;
+  mailbox mail;
+  transaction t;
+  
+  function new(mailbox mail);
+    this.mail=mail;
+  endfunction
+  
+  task gen;
+    t=new();
+    repeat(10)begin
+    t.randomize();
+    mail.put(t);
+     //$display("the value of wdata =%0d waddr=%0d",t.wdata,t.addr);
+      #20;
+    end
+    
+  endtask
+  
+endclass
