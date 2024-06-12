11d20a937bffc8a875e087ae6d305b4d092bb553
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 17:04:29 +0530

    Update test.sv

--- System_verilog/d_flipflop/test.sv
+++ System_verilog/d_flipflop/test.sv
@@ -1,1 +1,26 @@
+`include "environment.sv"
 
+class test;
+  
+  environment env;
+  
+  mailbox mail;
+  
+  virtual operation vintf;
+  
+  function new(virtual operation vintf,mailbox mail);
+    this.mail=mail;
+    this.vintf=vintf;
+  endfunction
+  
+  task memory;
+    env=new(vintf,mail);
+  endtask
+  
+  task run;
+    fork
+      env.memory;
+    env.run;
+    join
+  endtask
+endclass
