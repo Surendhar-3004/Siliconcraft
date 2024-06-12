5851680f080d5a3dddd1c4ab494933aabc5b3ccb
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 17:02:49 +0530

    Update driver.sv

--- System_verilog/d_flipflop/driver.sv
+++ System_verilog/d_flipflop/driver.sv
@@ -1,1 +1,26 @@
-
+class driver;
+  
+  transaction pkt;
+  
+  mailbox mail;
+  
+  virtual operation vintf;
+  
+  function new(virtual operation vintf,mailbox mail);
+    this.vintf=vintf;
+    this.mail=mail;
+  endfunction
+  
+  task drive;
+    
+    repeat(5)
+      begin
+        
+        pkt=new();
+        mail.get(pkt);
+        vintf.d=pkt.d;
+        pkt.q=vintf.q;
+      end
+  endtask
+endclass
+       
