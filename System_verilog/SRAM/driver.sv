36c1e9309a1e6f5357a0e5f27ff24e8caaefb7e0
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 15:36:51 +0530

    Update driver.sv

--- System_verilog/SRAM/driver.sv
+++ System_verilog/SRAM/driver.sv
@@ -1,1 +1,33 @@
+class driver;
+  virtual inter vi;
+  mailbox mail;
+    transaction t;
+    
+    function new(virtual inter vi,mailbox mail);
+      this.vi=vi;
+      this.mail=mail;
+    endfunction
+      
+      task drive;
+        repeat (10)begin
+           t=new();
+           mail.get(t);
+         
+          //#40 t.w_en=1'b0;
+          vi.wdata=t.wdata;
+          vi.addr=t.addr;
+          vi.w_en=1'b1;
+         // vi.w_en=t.w_en;
+          #20
+          vi.w_en=1'b0; 
+          t.rdata=vi.rdata;
+          //t.addr=vi.addr;
+         
+         // $display("the driver of wdata =%0d waddr=%0d",t.wdata,t.addr);
+          #20;
+        end
+      endtask
+  
+endclass
+
 
