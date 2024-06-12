56410f79b0411e9d273de5d870bd2be9bf3db1ae
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 17:07:30 +0530

    Update polymorphism.sv

--- System_verilog/SV codes/polymorphism/polymorphism.sv
+++ System_verilog/SV codes/polymorphism/polymorphism.sv
@@ -1,1 +1,34 @@
+// Code your design here
+class base_class;
+  virtual function void display();
+    $display("base_class");
+  endfunction
+endclass
 
+class ext_class_1 extends base_class;
+  function void display();
+    $display("ext_class_1");
+  endfunction
+endclass
+
+class ext_class_2 extends base_class;
+  function void display();
+    $display("ext_class_2");
+  endfunction
+endclass
+
+module class_polymorphism;
+  initial begin
+    
+    ext_class_1 ec_1=new();
+    ext_class_2 ec_2=new();
+    
+    base_class b_c[2];
+    
+    b_c[0]=ec_1;
+    b_c[1]=ec_2;
+    
+    b_c[0].display();
+    b_c[1].display();
+  end
+endmodule
