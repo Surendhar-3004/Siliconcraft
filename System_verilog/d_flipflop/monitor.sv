e733bc08c6849366c8d4281b25d1a36bb7a0cf04
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 17:03:33 +0530

    Update monitor.sv

--- System_verilog/d_flipflop/monitor.sv
+++ System_verilog/d_flipflop/monitor.sv
@@ -1,1 +1,28 @@
-
+class monitor;
+  
+  transaction pkt;
+  
+  mailbox mail;
+  
+  virtual operation vintf;
+  
+  function new(virtual operation vintf,mailbox mail);
+    this.vintf=vintf;
+    this.mail=mail;
+  endfunction
+  
+  task mon;
+    
+    repeat(5)
+      begin
+    
+    pkt=new();
+    
+    mail.put(pkt);
+    
+    pkt.d=vint.d;
+    pkt.q=vint.q;
+      end
+  endtask
+endclass
+    
