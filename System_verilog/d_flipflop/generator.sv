ab45271c9c87f24faf577dd476e9df17c4f6fcdd
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 17:02:23 +0530

    Update generator.sv

--- System_verilog/d_flipflop/generator.sv
+++ System_verilog/d_flipflop/generator.sv
@@ -1,1 +1,24 @@
-
+class generator;
+  
+  transaction pkt;
+  
+  mailbox mail;
+  
+  function new(mailbox mail);
+    this.mail=mail;
+  endfunction
+  
+  task gene;
+     
+    repeat(5)
+      begin
+        
+        pkt=new();
+        
+        pkt.randomize();
+        
+        mail.put(pkt);
+        
+      end
+  endtask
+endclass
