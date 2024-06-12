dc451f218d3f1757dff57091b4d88e34b2f75e79
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-12 17:04:00 +0530

    Update agent.sv

--- System_verilog/d_flipflop/agent.sv
+++ System_verilog/d_flipflop/agent.sv
@@ -1,1 +1,53 @@
+`include "transaction.sv"
+`include "driver.sv"
+`include "generator.sv"
+//`include "monitor.sv"
 
+class agent;
+  
+  transaction pkt;
+  
+  driver adrive;
+  
+  generator agen;
+  
+  //monitor amon;
+  
+  virtual operation vintf;
+
+  
+  mailbox mail;
+  
+  function new(virtual operation vintf,mailbox mail);
+    this.vintf=vintf;
+    this.mail=mail;
+  endfunction
+  
+  
+  
+  task memory;
+    
+    pkt=new();
+    adrive=new(vintf,mail);
+    agen=new(mail);
+  //  amon=new(vintf,mail);
+    
+  endtask
+  
+  task run;
+    
+    fork
+      agen.gene;
+      adrive.drive;
+    //  amon.mon;
+      
+      
+    join
+    
+  endtask
+endclass
+  
+  
+  
+  
+  
