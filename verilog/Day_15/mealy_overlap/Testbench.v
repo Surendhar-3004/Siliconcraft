7d1f03ae4e642f7d14ff4a91214548b17902bd05
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-25 12:37:52 +0530

    Update Testbench.v

--- verilog/Day_15/mealy_overlap/Testbench.v
+++ verilog/Day_15/mealy_overlap/Testbench.v
@@ -1,1 +1,31 @@
+ module mealyoverlaptb;
+  reg clk, rst, a;         
+  wire out;
+  
+  mealyoverlap uut (.clk(clk),.rst(rst),.a(a),.out(out));
+  initial begin
+    $dumpfile("mealyoverlap.vcd");
+    $dumpvars((1),mealyoverlaptb);
+  end
+  initial 
+    forever  #2 clk =~clk;
+    
+   initial begin
+    clk =0; 
+    rst = 0;
+    a=1;
+   #2 rst =1;
+   #4 a=1;
+   #4 a=0;
+   #4 a=1;
+   #4 a=0;
+     #30
+     $finish;
 
+   end
+    
+   initial begin
+    $monitor("%t input values : a=%b,clk =%b,rst =%b,output values : out=%b",$time,a,clk,rst,out);
+     
+  end
+endmodule
