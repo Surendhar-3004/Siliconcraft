d1bdfabc110bdd1c2c23b5f9651ed7d9c046df2f
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-25 14:15:51 +0530

    Update design.sv

--- System_verilog/Data_types/2state_datatype/design.sv
+++ System_verilog/Data_types/2state_datatype/design.sv
@@ -1,1 +1,30 @@
-
+// Code your design here
+module two_state_datatype;
+  
+  bit [2:0]bit_datatype;
+  byte byte_datatype;
+  shortint shortint_datatype;
+  int int_datatype;
+  longint longint_datatype;
+  initial begin
+  $display("before initialization of bit_datatype %b",bit_datatype);
+  $display("before initialization of byte_datatype %b",byte_datatype);
+    $display("before initialization of shortint_datatype %d",shortint_datatype);
+    $display("before initialization of int_datatype %d",int_datatype);
+    $display("before initialization of longint_datatype %d",longint_datatype);
+  
+  bit_datatype=3'b011;
+  byte_datatype=23;
+  shortint_datatype=56;
+  int_datatype=45;
+  longint_datatype=57;
+  
+  $display("after initialization of bit_datatype %b",bit_datatype);
+  $display("after initialization of byte_datatype %d",byte_datatype);
+  $display("after initialization of shortint_datatype %d",shortint_datatype);
+  $display("after initialization of int_datatype %d",int_datatype);
+    $display("after initialization of longint_datatype %b",longint_datatype);
+  end
+  
+endmodule
+  
