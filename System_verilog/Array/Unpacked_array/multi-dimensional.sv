4686ddba7cfcfa2cb78007c551fccde4b597b1c0
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-26 20:45:44 +0530

    Update multi-dimensional.sv

--- System_verilog/Array/Unpacked_array/multi-dimensional.sv
+++ System_verilog/Array/Unpacked_array/multi-dimensional.sv
@@ -1,1 +1,26 @@
+module multi_unpacked;
 
+  int abc[2][3];
+
+  initial begin
+   
+    foreach(abc[i]) begin
+      foreach(abc[i][j]) begin
+        abc[i][j] = $urandom_range(10,50);
+      end
+    end 
+   
+    $display("");
+    $display("// displaying values of 2 dimensional unpacked arrays");
+   
+    foreach(abc[i]) begin
+      foreach(abc[i][j]) begin
+        $display("values of abc[%0d][%0d] = %0d",i,j,abc[i][j]);
+      end
+    end
+
+    $display("");
+    $display("// displaying values of 2 dimensional unpacked array in one line");
+    $display("abc = %p",abc);
+    $display("");
+  end
