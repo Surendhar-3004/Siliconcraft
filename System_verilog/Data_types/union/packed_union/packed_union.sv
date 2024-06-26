f768005423700f5cb55ffcdca2b841735d965155
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-26 12:49:51 +0530

    Update packed_union.sv

--- System_verilog/Data_types/union/packed_union/packed_union.sv
+++ System_verilog/Data_types/union/packed_union/packed_union.sv
@@ -1,1 +1,22 @@
-
+module packed_union;
+  
+  union packed{
+    byte a;
+    byte b;
+  }pack_union;
+  
+  initial begin
+    
+    pack_union.a='h98;
+    
+    $display("\n a=%h",pack_union.a);
+    $display("\n b=%h",pack_union.b);
+    
+    pack_union.b='h34;
+    
+    $display("\n a = %0h", pack_union.a );
+    $display("\n b = %0h", pack_union.b );
+    $display("\n pack_union = %p", pack_union);
+    
+  end
+endmodule
