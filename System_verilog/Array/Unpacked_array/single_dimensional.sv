0257b371852c45cfe94c7c6eb66b731f2fcee0bd
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-26 20:30:49 +0530

    Update single_dimensional.sv

--- System_verilog/Array/Unpacked_array/single_dimensional.sv
+++ System_verilog/Array/Unpacked_array/single_dimensional.sv
@@ -1,1 +1,12 @@
-
+module single_dimensional_unpacked_array;
+  
+  int array1[4];
+  
+  initial begin
+    array1='{1,2,3,4};
+    
+    foreach(array1[i]) begin
+      $display("the value at array1[%0d] is:%0d",i,array1[i]);
+    end
+  end
+endmodule
