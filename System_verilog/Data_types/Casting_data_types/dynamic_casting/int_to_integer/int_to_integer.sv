dcb2d6323df3de3fb34cb89cb3309eeb82f02b7c
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-26 14:49:29 +0530

    Update int_to_integer.sv

--- System_verilog/Data_types/Casting_data_types/dynamic_casting/int_to_integer/int_to_integer.sv
+++ System_verilog/Data_types/Casting_data_types/dynamic_casting/int_to_integer/int_to_integer.sv
@@ -1,1 +1,11 @@
-
+module dynamic_int_integer;
+  
+  int int_data=53;
+  integer integer_data;
+  
+  initial begin
+    $cast(integer_data,int_data);
+    $display("integer_data=%d",integer_data);
+  end
+endmodule
+    
