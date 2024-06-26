d9e86f36ad074ad34dba4b7a6d97b34a18842681
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-26 12:34:29 +0530

    Update unpacked_union.sv

--- System_verilog/Data_types/union/unpacked_union/unpacked_union.sv
+++ System_verilog/Data_types/union/unpacked_union/unpacked_union.sv
@@ -1,1 +1,22 @@
-
+module unpacked_union;
+  
+  union{
+    byte a;
+    int b;
+  }unpack_union;
+  
+  initial begin
+    
+    unpack_union.b='hefcdba98;
+    
+    $display("b=%h",unpack_union.b)
+    $display("a=%h",unpack_union.a);
+    
+    unpack_union.a='h34;
+    
+    $display("\n a = %0h", unpack_union.a );
+    $display("\n a = %0h", unpack_union.b );
+    $display("\n unpack_union = %p", unpack_union);
+    
+  end
+endmodule
