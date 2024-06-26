c59f0a91da9f2d4f04b21bc5f689a73d1a547f04
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-26 10:22:23 +0530

    Update packed_structure.sv

--- System_verilog/Data_types/structure/packed_structure/packed_structure.sv
+++ System_verilog/Data_types/structure/packed_structure/packed_structure.sv
@@ -1,1 +1,22 @@
+struct packed {
+  bit[7:0] intr;
+  logic [23:0] addr;
+}str;
 
+module packed_structure;
+  
+  initial begin
+    str.intr='hff;
+    $display( "%t str=%h",$time,str);
+    $display( "%t str intr=%h",$time,str.intr);
+    
+  
+    str={8'h00,24'hFF_FF_FF};
+    $display( "%t str=%h",$time,str);
+    
+    str=0;
+    str=str+'h12;
+    $display( "%t str=%h",$time,str);
+    
+  end
+endmodule
