8cd3ce340d17ba3d29d2564d575cfce09bbaac72
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-27 20:22:18 +0530

    Update function_with_values_as_argument.sv

--- System_verilog/Function/function_with_values_as_argument.sv
+++ System_verilog/Function/function_with_values_as_argument.sv
@@ -1,1 +1,11 @@
-
+module function_with_value_as_argument;
+  int result=sum(4,5);
+  initial begin
+  $display("the answer is:%0d",result);
+  end
+  
+          function int sum(int var1,var2);    
+          $display("entered into function");   
+          return var1+var2;  
+          endfunction  
+endmodule 
