e17accc935ac3430cdce80d62f00a1c5414b9a67
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-28 07:06:34 +0530

    Update function_with_passing_an_array.sv

--- System_verilog/Function/function_with_passing_an_array.sv
+++ System_verilog/Function/function_with_passing_an_array.sv
@@ -1,1 +1,15 @@
+module function_with_passing_an_array;
+  initial begin
+  int array[5];
+  void '(func_arr(array));
+  $display("the elements are=%0p",array);
+           end
+           
+           function automatic int func_arr(ref int arr[5]);
+             foreach(arr[i])begin
+               arr[i]=i+2;
+             end
+             return 0;
+           endfunction
+           endmodule
 
