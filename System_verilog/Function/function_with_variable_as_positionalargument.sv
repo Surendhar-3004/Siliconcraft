ad756d0193fd356ae90fb5c3e6d95b2195ed18c8
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-28 06:29:39 +0530

    Update function_with_variable_as_positionalargument.sv

--- System_verilog/Function/function_with_variable_as_positionalargument.sv
+++ System_verilog/Function/function_with_variable_as_positionalargument.sv
@@ -1,1 +1,15 @@
 
+module function_with_variable_as_positional_argument;
+  int result;
+initial  
+          begin   
+          result=sum(.var1(5),.var2(6));  
+          $display("\treturned from function and");  
+          $display("\tstored the value of sum in result");  
+          $display("\n\t@ %0t ns, value of sum is %0d",$time,result);      
+          end  
+          function int sum(int var1,var2); 
+          $display("entered into function");   
+          return var1+var2;  
+          endfunction 
+endmodule
