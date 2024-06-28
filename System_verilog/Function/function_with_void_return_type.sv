4155f195091618797cae4c21a4390420a1516e09
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-28 07:13:12 +0530

    Update function_with_void_return_type.sv

--- System_verilog/Function/function_with_void_return_type.sv
+++ System_verilog/Function/function_with_void_return_type.sv
@@ -1,1 +1,10 @@
-
+module function_with_void_returntype;
+          initial  
+          begin  
+          display("\t ----output for function void return type-----");  
+          display("\t passing string to function for displaying");  
+          end  
+          function void display(string str);  
+          $display("%s",str);    
+          endfunction: display  
+endmodule
