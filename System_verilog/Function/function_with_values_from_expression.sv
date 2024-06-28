50eaae62f23a8ed73e97bc661a525a479d1df9cd
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-28 06:24:41 +0530

    Update function_with_values_from_expression.sv

--- System_verilog/Function/function_with_values_from_expression.sv
+++ System_verilog/Function/function_with_values_from_expression.sv
@@ -1,1 +1,13 @@
-
+module function_with_value_from_expression;
+    int result;
+  initial begin
+  
+    result=sum(4,5);
+    $display("the answer is:%0d",sum(4,5));
+  end
+  
+          function int sum(int var1,var2);    
+          $display("entered into function");   
+          return var1+var2;  
+          endfunction  
+endmodule
