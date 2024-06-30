b8cd1f84e90d153e23ee8071f2d7b29c0b6e3b98
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-06-30 15:23:37 +0530

    Update fork-disable_fork.sv

--- System_verilog/processes/fork-disable_fork/fork-disable_fork.sv
+++ System_verilog/processes/fork-disable_fork/fork-disable_fork.sv
@@ -1,1 +1,22 @@
+// Code your design here
+module threads();
+initial
+begin
+fork
+for(int H1_pointer=0; H1_pointer<2; H1_pointer++)begin
+  #1 $display ($time," First LOOP: Value of H1=%g", H1_pointer);
+end
 
+
+for(int D1_channel = 2; D1_channel > 0; D1_channel--)begin
+  #1 $display ($time," Second LOOP: Value of D1=%g", D1_channel);
+end
+join
+
+
+disable fork;
+$display("@%g Outside of FORK - JOIN\n",$time);#3 $finish;
+end
+endmodule
+
+  
