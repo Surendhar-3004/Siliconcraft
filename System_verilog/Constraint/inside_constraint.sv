41fe84c18611e0b0572e9ad62ebe876b8a5ed64f
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-08-29 16:31:05 +0530

    Update inside_constraint.sv

--- System_verilog/Constraint/inside_constraint.sv
+++ System_verilog/Constraint/inside_constraint.sv
@@ -1,1 +1,12 @@
+class base;
+ rand bit[8:0] A;
+constraint range { A inside {[0:63], [128:191]};}
+ endclass
 
+module test;
+  base h=new();
+initial repeat(10) begin
+ assert(h.randomize);
+  $display("The value is: %d",h.A);
+ end 
+endmodule
