5455291e809fdbc7edd3610d5aaffeb364a0b469
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-08-29 16:35:32 +0530

    Update soft_constraint.sv

--- System_verilog/Constraint/soft_constraint.sv
+++ System_verilog/Constraint/soft_constraint.sv
@@ -1,1 +1,12 @@
+class base;
+  rand bit[8:0] data;
+constraint soft_data_c { soft data == 0; }
+ endclass
 
+module test;
+  base h=new();
+initial repeat(10) begin
+ assert(h.randomize);
+  $display("The value is: %d",h.data);
+ end 
+endmodule
