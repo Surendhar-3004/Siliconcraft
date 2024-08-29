5aac5b97b98559a2750527b19897d8fbb0e1923f
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-08-29 17:08:17 +0530

    Update constraint_in_array.sv

--- System_verilog/Constraint/constraint_in_array.sv
+++ System_verilog/Constraint/constraint_in_array.sv
@@ -1,1 +1,21 @@
+// Code your design here
+class MyClass;
+rand bit [7:0] arr[5];
+  constraint array_c {
+ foreach (arr[i]) arr[i] inside {[10:100]};   }
+    function void display();
+        $display("Array values:");
+ foreach (arr[i]) $display("arr[%0d] = %0d", i, arr[i]);
+    endfunction
+endclass
+module tb;
+   initial begin
+        MyClass obj = new();
+if (obj.randomize()) begin
+            obj.display();
+     end else begin
+            $display("Randomization failed");
+  end
+    end
+endmodule
 
