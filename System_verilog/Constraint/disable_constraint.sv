c1cd9901cc0829992bbc1d69e45469e73cc2261c
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-08-29 16:48:10 +0530

    Update disable_constraint.sv

--- System_verilog/Constraint/disable_constraint.sv
+++ System_verilog/Constraint/disable_constraint.sv
@@ -1,1 +1,17 @@
+class Packet;
+  rand bit [3:0] addr;
+  rand bit [3:0] data;
+  constraint addr_c { data <= 10; }
+endclass
 
+module cons;
+  Packet pkt=new();
+  
+  initial begin
+    repeat(5) begin
+      pkt.randomize();
+      pkt.addr_c.constraint_mode(0);
+      $display("addr=%d data=%d",pkt.addr,pkt.data);
+    end
+  end
+endmodule
