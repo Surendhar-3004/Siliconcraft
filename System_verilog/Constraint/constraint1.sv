55cce5d11e9980a334ff114b0029967c7ced1dfb
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-08-29 15:30:53 +0530

    Update constraint1.sv

--- System_verilog/Constraint/constraint1.sv
+++ System_verilog/Constraint/constraint1.sv
@@ -1,1 +1,16 @@
+class Packet;
+rand bit [7:0] addr;
+rand bit [7:0] data;
+ constraint addr_c { addr < 128; }
+endclass
 
+module cons_1;
+  Packet pkt=new();
+
+  initial begin
+  repeat(5) begin
+      pkt.randomize();
+    $display("addr=%d data=%d",pkt.addr,pkt.data);
+    end
+  end
+endmodule
