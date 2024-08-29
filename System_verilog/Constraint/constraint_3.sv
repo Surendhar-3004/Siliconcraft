7acb7330827bb40abf2bed9b6922c47cde66ae14
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-08-29 16:19:28 +0530

    Update constraint_3.sv

--- System_verilog/Constraint/constraint_3.sv
+++ System_verilog/Constraint/constraint_3.sv
@@ -1,1 +1,18 @@
+class rand_vs_randc ; 
+	rand logic [1:0] data; 
+ 	randc logic [1:0] values ; 
+
+  function void post_randomize();  
+     $display(" data %h value %h ",data,values);
+endfunction
+endclass  	
+
+module test;  
+     rand_vs_randc p=new(); 
+   initial begin 
+repeat(10) 
+  p.randomize();
+     //p.post_randomize();
+end
+ endmodule 
 
