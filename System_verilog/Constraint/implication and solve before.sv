7caf345d395fc53e7a321b927bd67d689129928a
Surendhar-3004 <160817454+Surendhar-3004@users.noreply.github.com>
2024-08-29 16:59:18 +0530

    Update implication and solve before.sv

--- System_verilog/Constraint/implication and solve before.sv
+++ System_verilog/Constraint/implication and solve before.sv
@@ -1,1 +1,16 @@
+class ABC;
+ rand  bit a;
+rand  bit [1:0] b; 
+ constraint c_ab { a -> b == 3'h3; 
+	solve a before b;         }
+endclass
+module tb;
+  initial begin
+    ABC abc = new();
+  for (int i = 0; i < 8; i++) begin
+      abc.randomize();
+     $display ("a=%0d b=%0d", abc.a, abc.b);
+end
+  end
+endmodule
 
